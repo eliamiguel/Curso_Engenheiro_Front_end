cc08d061ce36ed92cb76df98b8cebcddb5c5eb68
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-05-30 21:41:54 -0300

    s

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,10 +22,6 @@ grunt.initConfig({
         files:['src/styles/**/*.less'],
         tasks:['less:development']
       },
-      html:{
-        files:['src/index.html'],
-        tasks:['replace:dev']
-      }
     },
     replace:{
       dev:{
@@ -40,15 +36,7 @@ grunt.initConfig({
             replacement: '../src/scripts/main.js'
           }
           ]
-        }, 
-        files:[
-          {
-            expand: true,
-            flatten: true,
-            src: ['src/index.html'],
-            dest:'dev/'
         }
-        ]
       }, 
       dist:{
         options:{
@@ -63,28 +51,8 @@ grunt.initConfig({
           }
           ]
         }, 
-        files:[
-          {
-            expand: true,
-            flatten: true,
-            src: ['prebuild/index.html'],
-            dest:'dist/'
-        }
-        ]
       }
-    }, 
-    htmlmin:{
-      dist:{
-        options:{
-          removeComments: true,
-          collapseWhitespace: true 
-        }, 
-        files:{
-        'prebuild/index.html': 'src/index.html'
-      }
-    }
-  }, 
-  clean:['prebuild'],
+  },
   uglify:{
     target:{
       files:{
@@ -97,12 +65,9 @@ grunt.initConfig({
 grunt.loadNpmTasks('grunt-contrib-less');
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-replace');
-grunt.loadNpmTasks('grunt-contrib-htmlmin');
-grunt.loadNpmTasks('grunt-contrib-clean');
 grunt.loadNpmTasks('grunt-contrib-uglify');
-
 grunt.registerTask('default', ['watch']);
-grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
+grunt.registerTask('build', ['less:production','replace:dist', 'uglify']);
 
 }
 
