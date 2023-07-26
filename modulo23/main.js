1b8864864a65794b7477709d700910fa6d074044
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-07-26 11:00:22 -0300

    Final

--- modulo23/main.js
+++ modulo23/main.js
@@ -1,15 +1,30 @@
-document.addEventListener("DOMContentLoaded", function(){
-document.getElementById("btn-buscar-cep").addEventListener('click', function(){
+// document.addEventListener("DOMContentLoaded", function(){
+// document.getElementById("btn-buscar-cep").addEventListener('click', function(){
 
-  const xhttp = new XMLHttpRequest();
-  const cep = document.getElementById('cep').value;
-  console.log(cep);
-  const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+//   const xhttp = new XMLHttpRequest();
+//   const cep = document.getElementById('cep').value;
+//   const endpoint = `https://viacep.com.br/ws/${cep}/json`;
 
-  xhttp.open("GET", endpoint);
-  xhttp.send();
+//   xhttp.open("GET", endpoint);
+//   xhttp.send();
 
+//   });
 
-  });
+// });
+$(document).ready(function(){
 
+$('#btn-buscar-cep').click( function(){
+
+    const cep = $('#cep').val();
+    const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+    $.ajax(endpoint).done(function(resposra){})
+
+  })
+$('#formulario-pedido').submit(function(evento){
+  evento.preventDefault();
+  if ($('#none').val().length==0){
+    throw new error('digite o nome');
+  }
+})
 })
