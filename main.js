0d60e79ee124170ac0de71a959816283ab87a811
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-07-31 21:31:52 -0300

    drd

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-  const image = document.querySelector('#image');
-  const nome = document.querySelector('#nome');
-  const sobrenome = document.querySelector('#sobrenome');
-  const repositorio = document.querySelector('#repositorio');
-  const seguidores = document.querySelector('#seguidores');
-  const seguindo= document.querySelector('#seguindo');
-  const link = document.querySelector('#link');
-
-  fetch('https://api.github.com/users/eliamiguel/')
-  .then( function(respost){
-    respost.json();
+  document.querySelector('#buscar').addEventListener('click', function(event){
+    event.preventDefault();
+  const pesquisar = document.querySelector('#pesquisar').value;
+    const image = document.querySelector('#image');
+    const nome = document.querySelector('#nome');
+    const sobrenome = document.querySelector('#sobrenome');
+    const repositorio = document.querySelector('#repositorio');
+    const seguidores = document.querySelector('#seguidores');
+    const seguindo= document.querySelector('#seguindo');
+    const link = document.querySelector('#link');
+  
+    fetch(`https://api.github.com/users/${pesquisar}`)
+    .then( function(respost){
+      return respost.json();
+    })
+    .then(function(json){
+    image.src =json.avatar_url;
+    nome.innerHTML = json.name;
+    sobrenome.innerHTML =json.login;
+    repositorio.innerHTML =json.public_repos;
+    seguidores.innerHTML = json.followers;
+    seguindo.innerHTML = json.following;
+    link.href=json.html_url;
   })
-  .then(function(json){
-  image.src =json.avatar_url;
-  nome = json.name;
-  sobrenome =json.login;
-  repositorio =json.public_repos;
-  seguidores = json.followers;
-  seguindo = json.following;
-  link.href=json.html_url;
-})
+  })
+
+  
 })
