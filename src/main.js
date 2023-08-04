1a18d3e66f9625d65059e057c38499c3ddfb7f84
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-08-04 00:30:40 -0300

    Final

---
+++ src/main.js
@@ -0,0 +1,44 @@
+const alunos= ['Elias Miguel']
+
+const AlunosNotas = alunos.map(function(nome){
+  return {
+    alunos:nome,
+    nota:8}
+})
+console.log("Todos os alunos ")
+AlunosNotas.push({nome: 'Maria Antunes',nota:7})
+AlunosNotas.push({ nome:'Tito Junior', nota:4})
+AlunosNotas.push({nome:'Julio da Sila',nota:6})
+AlunosNotas.push({ nome:'Paulo Alberto', nota:5})
+console.log(AlunosNotas)
+const verificarNota = AlunosNotas.filter(function(item){
+  return item.nota >=6
+})
+console.log("Alunos com a nota maior ou igual a 6 ")
+console.log(verificarNota)
+
+
+
+//  Esse é outro jeito que achei para resolver a atividade
+
+
+function alunosENotas(nome, nota){
+  this.nome = nome;
+  this.nota = nota
+}
+console.log("Todos os alunos ")
+const aluno =[]
+
+aluno.push( new alunosENotas ('Neves MIguel', 9));
+aluno.push(new alunosENotas ('Alvaros Manuel', 7));
+aluno.push(new alunosENotas('Maura beng', 6));
+aluno.push(new alunosENotas ('Tiago Luis', 4));
+aluno.push(new alunosENotas('José Armindo', 5));
+
+console.log(aluno)
+
+console.log("Alunos com a nota maior ou igual a 6 ")
+const verificarNotaMaiorOuIgual = aluno.filter(function(item){
+  return item.nota >=6
+});
+console.log(verificarNotaMaiorOuIgual)
