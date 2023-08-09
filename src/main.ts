dd47035198dac2180db4e2519e2fe208c972201c
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-08-08 21:04:39 -0300

    final

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+function Multiplicar(a:number, b:number):number {
+  return a * b
+}
+console.log(Multiplicar(12,36))
+
+const saudacao = (nome: string): string => 'Olá ' + nome;
+
+console.log(saudacao("Elias "))
